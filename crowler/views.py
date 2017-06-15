80615ed253a85efce949b578aa67c7c5e15072b8
Vlad Berest <deaddmindd@gmail.com>
2017-06-15 12:57:07 +0300

    commit

--- crowler/views.py
+++ crowler/views.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 from django.shortcuts import render
 import re
+
 # Create your views here.
 def main(request):
     html = urlopen("http://pchelomarket.com/15-inventar")
