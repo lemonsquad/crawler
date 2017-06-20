721734ddfbc051d715cbb759dfbc901406189598
Vlad Berest <deaddmindd@gmail.com>
2017-06-20 13:46:12 +0300

    for save 001

--- crowler/views.py
+++ crowler/views.py
@@ -4,15 +4,25 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 from django.shortcuts import render
 import re
+from .models import urls
   
 # Create your views here.
+
+def get_image_url (kwargs):
+	return kwargs
+
+
 def main(request):
-    html = urlopen("http://pchelomarket.com/15-inventar")
-    bsobj = bs(html)
+    html = urlopen("http://pchelomarket.com/15-inventar?id_category=15&n=57")
+    bsobj = bs(html, "html.parser")
 
     dict = {}
+    lists = []
+
     for link in bsobj.find("div",{"id":"center_column"}).findAll("a", {'product-name'}):
         if 'href' in link.attrs:
             dict[link.attrs['href']] = link.contents[0]
 
-    return render(request, 'crowler/index.html', {'dict':dict}) 
+            a = get_image_url(urlopen(link.attrs['href']))
+           
+    return HttpResponse(a)
